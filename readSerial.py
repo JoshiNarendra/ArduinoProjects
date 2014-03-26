33261d645a630c002b822d748577feb86eb25542
ujamaar <narendra.r.joshi@gmail.com>
2014-03-26 01:11:49 -0400

    Update readSerial.py

--- readSerial.py
+++ readSerial.py
@@ -7,4 +7,8 @@ ser = serial.Serial('/dev/ttyACM0',9600);
 while True:
 	currentInput = ser.readline();
 	print currentInput;
-	dataLog.write(currentInput);	
+	dataLog.write(currentInput);
+	
+	
+	//just to see if git edits can be done online. 
+	//ae rajai lai bari lai
